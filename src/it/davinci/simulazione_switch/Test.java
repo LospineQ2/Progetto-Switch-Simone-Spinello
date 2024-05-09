6c4f015074aa8877b7090ab42e129ac60589d4c8
Simone Spinello <spinosimo06@gmail.com>
2024-05-09 13:33:26 +0200

    Ultimatum progetto Unica

--- src/it/davinci/simulazione_switch/Test.java
+++ src/it/davinci/simulazione_switch/Test.java
@@ -1,52 +1,53 @@
 package it.davinci.simulazione_switch;
-
-import java.util.Vector;
+import java.util.Random;
 
 public class Test
 {
     public static void main(String[] args)
     {
-        /*
-
-            MAIN CON INSERIMENTO FRAME PRECEDENTE
-
-         */
-        Porta pA=new Porta("A");
-        Porta pB=new Porta("B");
-        int msg=100;
+        Frame f;
+        Random rand = new Random();
+        Porta portaA=new Porta("A");
+        Porta portaB=new Porta("B");
+        int p=8,q;
         int time=10;
-        int j=0;
+        int totalFrame=0;
 
         Integer sommaCodaA[]={0,0,0};
 
         Integer sommaCodaB[]={0,0,0};
 
-        for (int i=0;i<msg;i++)
+        for (int i=0;i<time;i++)
         {
-            Frame f=new Frame();
-            if(f.getMac().equals("A"))pA.addFrame(f);
-            else pB.addFrame(f);
-
-        }
-        pA.smistaPorta();
-        pB.smistaPorta();
-
-        while (j<time)
-        {
-            if (pA.uscitaPorta())
+            for (int j=0;j<1+rand.nextInt(11);j++)//numero randomico di frame generati in un'unità di tempo
             {
-                sommaCodaA[0] += pA.getP1size();
-                sommaCodaA[1] += pA.getP2size();
-                sommaCodaA[2] += pA.getP3size();
+                q=rand.nextInt(10)-p;
+                if(q<=p)//probabilità esistenza di un frame
+                {
+                    f = new Frame();
+                    if (f.getMac().equals("A")) portaA.addFrame(f);
+                    else portaB.addFrame(f);
+                    totalFrame++;
+                }
             }
-            if (pB.uscitaPorta())
-            {
-                sommaCodaB[0] += pB.getP1size();
-                sommaCodaB[1] += pB.getP2size();
-                sommaCodaB[2] += pB.getP3size();
+            portaA.smistaPorta();
+            portaB.smistaPorta();
+
+            if(portaA.uscitaPorta()){
+                sommaCodaA[0]+= portaA.getP1size();
+                sommaCodaA[1]+= portaA.getP2size();
+                sommaCodaA[2]+= portaA.getP3size();
             }
-            j++;
+            if(portaB.uscitaPorta()){
+                sommaCodaB[0]+= portaB.getP1size();
+                sommaCodaB[1]+= portaB.getP2size();
+                sommaCodaB[2]+= portaB.getP3size();
+            }
         }
+
+        System.out.println("\nTotale pacchetti generati in "+time+" unità di tempo:"+totalFrame+"\n");
+
+
         System.out.println("Nella porta A");
         System.out.println("Lunghezza media in Buffer 3 (Maggiore Priorità): "+sommaCodaA[2]/time);
         System.out.println("Lunghezza media in Buffer 2: "+sommaCodaA[1]/time);
